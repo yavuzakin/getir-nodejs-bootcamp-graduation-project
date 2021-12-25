ca1b0fe8bfc371912cf1bed56d0a523ca9b01347
yavuzakin <yavuzakin55@gmail.com>
2021-12-25 15:57:38 +0300

    mongodb url read from dotenv

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -4,14 +4,10 @@ const db = mongoose.connection;
 
 db.once('open', () => {
     console.log('DB connection is successfull');
-
-    // mongoose.connection.db.collection('records').find().toArray(function(e, d) {
-    //     console.log(d);
-    // })
 });
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true', {
+    await mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
