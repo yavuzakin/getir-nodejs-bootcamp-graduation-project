cc2634f2722e993215f069eeeae4d7892e28aa95
yavuzakin <yavuzakin55@gmail.com>
2021-12-26 16:58:18 +0300

    Undefined endpoints error handling implemented

--- src/app.js
+++ src/app.js
@@ -2,14 +2,27 @@ const express = require('express');
 const loaders = require('./loaders');
 const config = require('./config');
 const recordRouter = require('./routes/recordRoutes');
+const ApiError = require('./scripts/utils/ApiError');
+const errorHandler = require('./middlewares/errorHandler');
 
 config();
 loaders();
 
 const app = express();
 
+// Body parser, reading data from body into req.body
 app.use(express.json());
-app.use('/api/records', recordRouter);
+
+// Route
+app.use('/api/v1/records', recordRouter);
+
+// Catch undefined routes and throw an error
+app.all('*', (req, res, next) => {
+    next(new ApiError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global error handler
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
