70cbbf19c757d81e84b5d4b7d325943b3d8458ac
yavuzakin <yavuzakin55@gmail.com>
2021-12-26 18:08:21 +0300

    mongodb connection results are logged to corresponding files using winston

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
-
-const db = mongoose.connection;
-
-db.once('open', () => {
-    console.log('DB connection is successfull');
-});
+const loggerSuccess = require('./../scripts/logger/application');
+const loggerError = require('./../scripts/logger/error');
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        // Successfull console log
+        console.log('DB connection is successfull');
+        // Successfull file log
+        loggerSuccess.log({
+            level: 'info',
+            message: 'Connected to DB successfully'
+        });
+    } catch(err) {
+        // Error console log
+        console.log('DB connection error!');
+        // Error file log
+        loggerError.log({
+            level: 'error',
+            message: `Could not connect to DB. ${err.message}`
+        });
+    }
 }
 
 module.exports = {
