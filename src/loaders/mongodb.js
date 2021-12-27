5981458df894e3b38230ee76bc2de5766d6cd9ab
yavuzakin <yavuzakin55@gmail.com>
2021-12-27 20:49:38 +0300

    mongodb connection updated

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useMongoClient: true,
         });
 
         // Successfull file log, it logs to console also
