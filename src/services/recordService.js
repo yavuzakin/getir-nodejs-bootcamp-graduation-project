3a8129e28299aef24483aaa768b7f045d8d99b51
yavuzakin <yavuzakin55@gmail.com>
2021-12-27 16:55:32 +0300

    Function documentation added

--- src/services/recordService.js
+++ src/services/recordService.js
@@ -1,10 +1,18 @@
 const Record = require('./../models/Record');
 
+/** 
+* Filters the Record documents, group them by the key, sums the count array, sorts by totalCount
+* @param {Date Object} startDate The earliest date createdAt field can be
+* @param {Date Object} endDate The latest date createdAt field can be
+* @param {Number} minCount The minimum value for the totalCount field
+* @param {Number} maxCount The maximum value for the totalCount field
+* @return {Aggregate Object} Aggregation pipeline object, needs to be awaited for the expected result 
+*/
 exports.getFilteredRecords = (startDate, endDate, minCount, maxCount) => {
     return Record.aggregate([
         // First filter createdAt by startDate and endDate
         {
-            $match: { $and: [ { createdAt: { $gt: startDate } }, { createdAt: { $lt: endDate } } ] }
+            $match: { $and: [ { createdAt: { $gte: startDate } }, { createdAt: { $lte: endDate } } ] }
         },
         // Create one document per one element in count array by extracting count elements from array
         {
@@ -26,6 +34,10 @@ exports.getFilteredRecords = (startDate, endDate, minCount, maxCount) => {
         // Filter the totalCount by minCount and maxCount
         {
             $match: { $and: [ { totalCount: { $gte: minCount } }, { totalCount: { $lte: maxCount } } ] }
+        },
+        // Sort by totalCount in ascending order
+        {
+            $sort: { totalCount: 1 }
         }
     ]);
 }
